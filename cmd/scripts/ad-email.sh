6997d5289aa0b2182f845113ea35a617ea9bf603
juxuny <juxuny@gmail.com>
2021-06-11 22:07:04 +0800

    ad-email send script

--- cmd/scripts/ad-email.sh
+++ cmd/scripts/ad-email.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source cmd/scripts/base.env
 source cmd/scripts/.env
 # shellcheck disable=SC2046
 #eval $(go run github.com/juxuny/data-utils/cmd/ad-email set-proxy --user=${PROXY_USER} --pass=${PROXY_PASS})
